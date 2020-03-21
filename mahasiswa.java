8a224e3c5885630c590524daabae3a0bb9d76ec8
Risnandahm <55829537+Risnandahm@users.noreply.github.com>
2020-03-21 08:24:18 +0700

    Add files via upload

---
+++ mahasiswa.java
@@ -0,0 +1,15 @@
+package tugas;
+
+public class Mahasiswa {
+    
+    public static void main(String[] args) {
+      String nama = "Risnanda Hasna Mufida";
+      String kelas = "4A";
+      String nim = "18090092";
+        System.out.println("Nama : "+nama);
+        System.out.println("Kelas : "+kelas);
+        System.out.println("Nim : "+nim);
+        
+    }
+    
+}
